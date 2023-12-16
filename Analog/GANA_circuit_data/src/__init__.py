82090c4dc15270acff14d5debaa23c864708dae8
YiKangOY <elvinlothar@gmail.com>
2023-12-16 11:15:34 +0800

    removing GANA for now

--- Analog/GANA_circuit_data/src/__init__.py
+++
@@ -1,6 +0,0 @@
-#from BasicElement import *
-#from read_netlist import *
-#from coarsening import *
-#from graph import *
-#from . import *
-__all__ = ["BasicElement", "coarsening", "graph",  "models", "read_netlist",  "utils", "spiceParser"]
